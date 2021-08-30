a86483fd598e7de8dbb60edc83efd66f397a0051
Andrew Dansby <andydansby@gmail.com>
2021-08-30 07:07:22 -0400

    Add files via upload

---
+++ UNCONTENDED/uncontended.c
@@ -0,0 +1,62 @@
+// using sccz80 1.99c
+
+#include <arch/zx.h>
+#include <stdio.h>//standard input output
+#include <input.h>//used for detecting key inputs
+#include <intrinsic.h>//temp to place labels
+//powerful troubleshooting tool
+        //intrinsic_label(label_start);
+        //intrinsic_label(label_end);
+
+#include "variables.h"
+#include "externs.h"
+#include "uncontended.h"
+
+
+void main ()
+{
+    zx_cls(PAPER_WHITE);
+
+    //intrinsic_label(border_start);
+    zx_border(INK_WHITE);
+    //intrinsic_label(border_end);
+    //uncomment both labels and look at map to find address
+    // then set breakpoints in your favorite emulator to see
+    // the resulting assembly code
+
+    printf("\x16\x01\x02");
+    printf ("Main () running in uncontended\n");
+
+    printf ("_Variable_in_contended = %d\n", Variable_in_contended);
+
+    printf ("_Variable_in_Bank0 = %d\n", Variable_in_Bank0);
+
+    printf ("tom = %d\n", tom);
+    printf ("dick = %d\n", dick);
+    printf ("harry = %d\n", harry);
+    printf ("eve = %d\n", eve);
+    printf ("bob = %d\n\n", bob);
+
+
+    harry = add_two_numbers (tom, dick);
+    printf ("Add 2 numbers %d + %d = %d\n", tom, dick, harry);
+
+    harry = subtract2numbers (Variable_in_Bank0, eve);
+    printf ("Subtract 2 numbers %d - %d = %d\n", Variable_in_Bank0, eve, harry);
+
+    harry = multiply2numbers (Variable_in_contended, bob);
+    printf ("Multiply 2 numbers %d * %d = %d\n", Variable_in_contended, bob, harry);
+
+    harry = add_two_numbers (Variable_in_contended, Variable_in_Bank0);
+    printf ("Add 2 numbers %d + %d = %d\n", Variable_in_contended, Variable_in_Bank0, harry);
+
+    printf("\n\nPress 0-7 for Border Test\n");
+
+    while(1)
+    {
+        Border_test();
+    }
+}
+
+
+//must have blank line at end
